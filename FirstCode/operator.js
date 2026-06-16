391fec5e9c139fce8ae1db0faf1c141ec42b836b
Jyotiie <singlajyoti05@gmail.com>
2026-06-16 19:11:59 +0530

    codi

---
+++ FirstCode/operator.js
@@ -0,0 +1,23 @@
+//Arithmetic operators
+// let a = 5;
+// let b = 6;
+
+// console.log("a+b"); // Addition
+
+//Assignment operators
+
+// let a = 5;
+// let b = 2;
+
+// a += 4; // a = a + 4
+// console.log("a=", a);
+
+let num = prompt("Enter a number: ");
+
+
+if (num % 5 == 0) {
+    console.log(num + " is divisible by 5");
+}
+else {
+    console.log(num + " is not divisible by 5");
+}
