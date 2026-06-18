af59e175741146e08e883ce20dc0f63e247cd22d
Jyotiie <singlajyoti05@gmail.com>
2026-06-18 19:50:02 +0530

    string

---
+++ FirstCode/string.js
@@ -0,0 +1,16 @@
+//strings
+// let str1 = "Hello";
+// let str2 = "World";
+// let str3 = str1 + " " + str2; //concatenation
+// console.log(str3); //Output: Hello World
+
+//Template literals
+// let sentence = `Hello Jyoti`;
+// console.log(typeof sentence);
+
+let obj = {
+    item: "pen",
+    price: 10,
+};
+
+console.log("the cost of", obj.item, "is", obj.price);
