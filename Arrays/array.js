0634306854940fdb9c3bfc59e76e02d5bc265fb9
Jyotiie <singlajyoti05@gmail.com>
2026-06-19 15:42:01 +0530

    new

---
+++ Arrays/array.js
@@ -0,0 +1,14 @@
+// let marks = [97, 82, 75, 64, 36];
+// console.log(marks);
+// console.log(marks.length);//property
+
+let heroes = ["ironman", "thor", "hulk", "shaktiman", "spiderman", "antman"];
+
+// for (let idx = 0; idx < heroes.length; idx++) {
+//     console.log(heroes[idx]);
+// }
+
+//for of elements
+for (let hero of heroes) {
+    console.log(hero);
+}
