a12fbbef21f493b5b5202404a9eeffff89e9a59f
Jyotiie <singlajyoti05@gmail.com>
2026-06-22 19:42:34 +0530

    Dom

---
+++ DOM/new.js
@@ -0,0 +1,18 @@
+
+
+let para=document.querySelector("p");
+console.log(para.getAttribute("class"));
+
+
+
+let newBtn=document.createElement("button");
+newBtn.innerText="click me";
+console.log(newBtn);
+
+let div=document.querySelector("div");
+div.append(newBtn);
+
+let newHeading=document.createElement("hi");
+newHeading.innerHTML="<i>Hi, I am new!</i>";
+
+document.querySelector("body").prepend(newHeading);
