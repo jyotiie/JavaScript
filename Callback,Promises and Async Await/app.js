2afa0e4fcab1318b58b51aa96609eabe29d7ff07
Jyotiie <singlajyoti05@gmail.com>
2026-07-04 15:32:18 +0530

    Callback,Promises and Async Await

---
+++ Callback,Promises and Async Await/app.js
@@ -0,0 +1,5 @@
+function hello(){
+    console.log("hello");
+}
+
+setTimeout(hello,2000) //timeout ; 2s=2000ms
---
+++ Callback,Promises and Async Await/app3.js
@@ -0,0 +1,32 @@
+// let promise=new Promise((resolve,reject) => {
+//     console.log("I am a promise");
+//     resolve(123);
+// });
+
+// function getData(dataId,getNextData){
+//     return new Promise((resolve,reject) => {
+//         setTimeout(() => {
+//             console.log("data", dataId);
+//             resolve("success");
+//             if(getNextData){
+//                 getNextData();
+//             }
+//         }, 5000);
+//     });
+// }
+
+const getPromise= () => {
+    return new Promise((resolve,reject) => {
+        console.log("I am a promise");
+        resolve("success");
+    });
+};
+
+let promise=getPromise();
+promise.then((res) => {
+    console.log("promise fulfilled",res);
+});
+
+promise.catch((err) => {
+    console.log("rejected",err);
+});
